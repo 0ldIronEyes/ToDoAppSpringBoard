369929676a6db378788d954669febefc57c38a82
0ldIronEyes <running.to.mars@gmail.com>
2023-07-18 16:10:20 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+const formElement = document.querySelector("form");
+formElement.addEventListener("submit", function(ev) {
+    ev.preventDefault();
+
+})
+
+
+function addTask() {
+    
+    const taskText = document.getElementById("taskInput");
+    const taskList = document.getElementById("taskList");
+    
+    
+    if (taskText !== "") {
+      let li = document.createElement("li");
+      li.innerText = taskText.value;
+      li.className = "listelement";
+      let deletebutton = document.createElement("button");
+      deletebutton.classList.add("listbutton");
+      deletebutton.innerText = "delete";
+      li.appendChild(deletebutton);
+      taskList.appendChild(li);
+      taskInput.value = "";
+    }
+  }
